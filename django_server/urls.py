410dcbc0be99bb0521294dfcf8cfd9e714128df1
Utkarsh Bhardwaj <utkarshbhardwajmail@gmail.com>
2026-01-08 22:46:36 +0530

    fix: basic fix and practice sheet

--- django_server/urls.py
+++ django_server/urls.py
@@ -5,6 +5,5 @@ from service.views.healthcheck import HealthCheckView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HealthCheckView.as_view(), name='Healthcheck'),
     path('api/v1/', include('service.urls_v1'))
 ]
